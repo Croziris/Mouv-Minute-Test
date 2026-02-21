f642013ab7b511eff03b05dc4048b815b57191b4
Croziris <crz.pierre13@gmail.com>
2026-02-21 18:38:38 +0100

    Changement pour N8N proxy

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,5 @@
 
 interface ImportMetaEnv {
   readonly VITE_POCKETBASE_URL: string;
-  readonly VITE_NOTION_API_KEY: string;
-  readonly VITE_NOTION_DB_ID: string;
-  readonly VITE_NOTION_ALLOWED_APP?: string;
+  readonly VITE_NOTION_PROXY_BASE_URL: string;
 }
